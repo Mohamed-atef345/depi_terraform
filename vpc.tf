7c4ff9cad54c72136528e0b51c93f7076d37b88d
Mohamed-atef345 <ma4243030@gmail.com>
2025-03-15 15:29:51 +0200

    edited vpc.tf file

--- vpc.tf
+++ vpc.tf
@@ -52,19 +52,20 @@ resource "aws_security_group" "allow_http" {
 resource "aws_vpc_security_group_ingress_rule" "security_ingress_rule" {
   security_group_id = aws_security_group.allow_http.id
 
-  cidr_ipv4   = "0.0.0.0/0"
+  cidr_ipv4   = ["0.0.0.0/0"]
   from_port   = 80
   ip_protocol = "tcp"
   to_port     = 80
 }
 
-resource "aws_vpc_security_group_egress_rule" "example" {
+resource "aws_vpc_security_group_egress_rule" "security_egress_rule" {
   security_group_id = aws_security_group.allow_http.id
 
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 80
-  ip_protocol = "tcp"
-  to_port     = 80
+  cidr_ipv4   = ["0.0.0.0/0"]
+  from_port   = 0
+  ip_protocol = "-1"
+  to_port     = 0
 }
+
 
 
